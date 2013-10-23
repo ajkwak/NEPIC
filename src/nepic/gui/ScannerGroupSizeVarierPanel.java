839a9afaec64b7617de6114039cfcdccfbf408b1
ajkwak <aj@kwak.fake>
2013-10-22 20:31:28 -0700

    Allow manual adjustment of preprocessing group size for scanline data

---
+++ src/nepic/gui/ScannerGroupSizeVarierPanel.java
@@ -0,0 +1,58 @@
+package nepic.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import nepic.roi.OneDimensionalScanner;
+import nepic.util.Verify;
+
+public class ScannerGroupSizeVarierPanel extends JPanel {
+    private static final long serialVersionUID = 1L; // Default
+    private Graph graph;
+    private OneDimensionalScanner scanner;
+    private JTextField groupSizeTF;
+
+    public ScannerGroupSizeVarierPanel(OneDimensionalScanner scanner) {
+        Verify.notNull(scanner, "OneDimensionalScanner");
+        this.scanner = scanner;
+        int graphWidth = 800;
+        int graphHeight = 400;
+
+        graph = new Graph(800, 400, 0x000000 /* Black */).setData(scanner.getGraphData());
+        graph.setLocation(0, 0);
+        graph.setVisible(true);
+        add(graph);
+
+        groupSizeTF = new JTextField(50);
+        // groupSizeTF.setEditable(true);
+        // groupSizeTF.setSize(50, 25);
+        groupSizeTF.setLocation(graphWidth - 55, graphHeight + 5);
+        groupSizeTF.setVisible(true);
+        groupSizeTF.addActionListener(new SetGroupSizeHandler());
+        add(groupSizeTF);
+
+        // setLayout(null);
+        setBorder(UtilityMethods.PANEL_BORDERS);
+        this.setSize(graphWidth + 5, graphHeight + 45);
+        this.setMinimumSize(getSize());
+        this.setPreferredSize(getSize());
+        setVisible(true);
+    }
+
+    private class SetGroupSizeHandler implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                int groupSize = Integer.parseInt(groupSizeTF.getText());
+                scanner.smoothData(groupSize);
+                graph.redraw(true, true, true);
+                repaint();
+            } catch (NumberFormatException nfe) {
+                groupSizeTF.setText("");
+            }
+
+        }
+    }
+}
