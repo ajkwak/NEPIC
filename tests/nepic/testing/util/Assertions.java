6c5b95679f1f427eef9161f74eab68a8a37c7ded
ajkwak <ajkwak@users.noreply.github.com>
2014-01-29 16:19:15 -0800

    Add assertions with useful fail messages for using in JUnit tests.  These assertions will act as an adjunct to the JUnit Assert class.

---
+++ tests/nepic/testing/util/Assertions.java
@@ -0,0 +1,38 @@
+package nepic.testing.util;
+
+import junit.framework.AssertionFailedError;
+
+/**
+ * This class contains a collection of static assert methods with helpful fail errors. This class
+ * acts as an adjunct to the {code org.junit.Assert} class.
+ *
+ * @author AJ Parmidge
+ */
+public class Assertions {
+
+    /**
+     * Asserts that the given string contains the given substring (case-sensitive).
+     * 
+     * @param string the string to check
+     * @param substring the substring to check
+     */
+    public static void assertContains(String string, String substring) {
+        if(!string.contains(substring)){
+            throw new AssertionFailedError("Expected \"" + string + "\" to contain substring \""
+                    + substring + "\"");
+        }
+    }
+
+    /**
+     * Asserts that the given string contains the given substring (NOT case-sensitive).
+     * 
+     * @param string the string to check
+     * @param substring the substring to check
+     */
+    public static void assertContainsIgnoreCase(String string, String substring) {
+        if (!string.toLowerCase().contains(substring.toLowerCase())) {
+            throw new AssertionFailedError("Expected \"" + string + "\" to contain substring \""
+                    + substring + "\"");
+        }
+    }
+}
---
+++ tests/nepic/testing/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains utility classes that assist in unit testing.
+ *
+ * @author AJ Parmidge
+ */
+package nepic.testing.util;
