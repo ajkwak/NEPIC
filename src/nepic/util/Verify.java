f8f034c015ce24524b63d296af0711f3f8635ad8
ajkwak <ajkwak@users.noreply.github.com>
2014-01-29 16:01:23 -0800

    Improve JUnit tests for Verify class

--- src/nepic/util/Verify.java
+++ src/nepic/util/Verify.java
@@ -59,25 +59,25 @@ public class Verify {
 
     /**
      * Verifies that the given {@link Collection} is non-empty.
-     * 
+     *
      * @param toCheck the collection to check.
      * @throws NullPointerException if the given collection is {@code null}
      * @throws IllegalArgumentException if the given collection is empty
      */
     public static void nonEmpty(Collection<?> toCheck) {
-        Verify.nonEmpty(toCheck, "given iterable");
+        Verify.nonEmpty(toCheck, "given collection");
     }
 
     /**
      * Verifies that the given {@link Collection} is non-empty.
-     * 
+     *
      * @param toCheck the collection to check.
      * @param item the name of the collection variable
      * @throws NullPointerException if the given collection is {@code null}
      * @throws IllegalArgumentException if the given collection is empty
      */
     public static void nonEmpty(Collection<?> toCheck, String item) {
-        String msg = "Expected " + item + " to be non-empty.  Instead got " + item;
+        String msg = "Expected " + item + " to be non-empty collection.";
         Verify.notNull(toCheck, msg);
         Verify.argument(!toCheck.isEmpty(), msg);
     }
@@ -90,7 +90,7 @@ public class Verify {
      * @throws IllegalArgumentException if the given string is empty
      */
     public static void nonEmpty(String toCheck) {
-        Verify.nonEmpty(toCheck, "given String");
+        Verify.nonEmpty(toCheck, "given string");
     }
 
     /**
@@ -131,6 +131,7 @@ public class Verify {
         int i = 0;
         for (Object element : toCheck) {
             Verify.notNull(element, "Element " + i + " of " + item);
+            i++;
         }
     }
 }
--- tests/nepic/util/VerifyTest.java
+++ tests/nepic/util/VerifyTest.java
@@ -1,5 +1,7 @@
 package nepic.util;
 
+import static nepic.testing.util.Assertions.assertContains;
+import static nepic.testing.util.Assertions.assertContainsIgnoreCase;
 import static org.junit.Assert.*;
 
 import java.awt.Point;
@@ -19,19 +21,18 @@ import com.google.common.collect.Lists;
 public class VerifyTest {
 
     @Test
-    public void notNull_NullObject_Throws() {
+    public void notNull_nullObject_throwsWithMessage() {
         String message = "Here's my message!";
         try {
             Verify.notNull(null, message);
             fail("Expected NullPointerException");
         } catch (NullPointerException expected) {
-            // MoreAsserts.assertContains();
             assertEquals(message, expected.getMessage());
         }
     }
 
     @Test
-    public void notNull_NonNullObject_Succeeds() {
+    public void notNull_nonNullObject_succeeds() {
         // None of these should throw.
         Verify.notNull("", "Message");
         Verify.notNull(new Point(9, 8), "Message");
@@ -40,7 +41,7 @@ public class VerifyTest {
     }
 
     @Test
-    public void argument_False_Throws() {
+    public void argument_false_throwsWithMessage() {
         String message = "Hello, world!";
         try {
             Verify.argument("" == "Voila", message);
@@ -51,7 +52,7 @@ public class VerifyTest {
     }
 
     @Test
-    public void argument_True_Succeeds() {
+    public void argument_true_succeeds() {
         // None of these should throw.
         Verify.argument(true, "Message");
         Verify.argument(2 * 6 == new Integer(13) - 1, "Message");
@@ -59,7 +60,7 @@ public class VerifyTest {
     }
 
     @Test
-    public void state_False_Throws() {
+    public void state_false_throwsWithMessage() {
         String message = "Hello, world!";
         try {
             Verify.state("" == "Voila", message);
@@ -70,7 +71,7 @@ public class VerifyTest {
     }
 
     @Test
-    public void state_True_Succeeds() {
+    public void state_true_succeeds() {
         // None of these should throw.
         Verify.state(true, "Message");
         Verify.state(2 * 6 == new Integer(13) - 1, "Message");
@@ -78,50 +79,174 @@ public class VerifyTest {
     }
 
     @Test(expected = NullPointerException.class)
-    public void nonEmpty_NullCollection_Throws() {
+    public void nonEmpty_nullCollection_throws() {
         Verify.nonEmpty((Collection<?>) null);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void nonEmpty_EmptyCollection_Throws() {
-        Verify.nonEmpty(new ArrayList<String>());
+    @Test
+    public void nonEmpty_nullCollection_throwsWithMessage() {
+        String collectionName = "My Collection";
+        try {
+            Verify.nonEmpty((Collection<?>) null, collectionName);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException expected) {
+            assertContains(expected.getMessage(), collectionName);
+        }
     }
 
     @Test
-    public void nonEmpty_NonEmptyCollection_Succeeds() {
+    public void nonEmpty_emptyCollection_throws() {
+        try {
+            Verify.nonEmpty(new ArrayList<String>());
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException expected) {
+            assertContainsIgnoreCase(expected.getMessage(), "empty");
+        }
+    }
+
+    @Test
+    public void nonEmpty_emptyCollection_throwsWithMessage() {
+        String collectionName = "My Collection";
+        try {
+            Verify.nonEmpty(new ArrayList<String>(), collectionName);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException expected) {
+            String message = expected.getMessage();
+            assertContains(message, collectionName);
+            assertContainsIgnoreCase(message, "empty");
+        }
+    }
+
+    @Test
+    public void nonEmpty_nonEmptyCollection_succeeds() {
+        // Call method without message.
         Verify.nonEmpty(Lists.newArrayList("Hello"));
         Verify.nonEmpty(Lists.newArrayList("Hello", "World"));
+
+        // Call method with message.
+        Verify.nonEmpty(Lists.newArrayList("Hello"), "My Collection");
+        Verify.nonEmpty(Lists.newArrayList("Hello", "World"), "My Collection");
     }
 
     @Test(expected = NullPointerException.class)
-    public void nonEmpty_NullString_Throws() {
+    public void nonEmpty_nullString_throws() {
         Verify.nonEmpty((String) null);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void nonEmpty_EmptyString_Throws() {
-        Verify.nonEmpty("");
+    @Test
+    public void nonEmpty_nullString_throwsWithMessage() {
+        String stringName = "My String";
+        try {
+            Verify.nonEmpty((String) null, stringName);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException expected) {
+            assertContains(expected.getMessage(), stringName);
+        }
     }
 
     @Test
-    public void nonEmpty_NonEmptyString_Succeeds() {
+    public void nonEmpty_emptyString_throws() {
+        try {
+            Verify.nonEmpty("");
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException expected) {
+            assertContainsIgnoreCase(expected.getMessage(), "empty");
+        }
+    }
+
+    @Test
+    public void nonEmpty_emptyString_throwsWithMessage() {
+        String collectionName = "My Collection";
+        try {
+            Verify.nonEmpty(new ArrayList<String>(), collectionName);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException expected) {
+            String message = expected.getMessage();
+            assertContains(message, collectionName);
+            assertContainsIgnoreCase(message, "empty");
+        }
+    }
+
+    @Test
+    public void nonEmpty_nonEmptyString_succeeds() {
+        // Call method without message.
         Verify.nonEmpty(" ");
         Verify.nonEmpty("a");
         Verify.nonEmpty("Hello world!");
+
+        // Call method with message.
+        Verify.nonEmpty(" ", "My String");
+        Verify.nonEmpty("a", "My String");
+        Verify.nonEmpty("Hello world!", "My String");
     }
 
     @Test(expected = NullPointerException.class)
-    public void noNullElements_NullCollection_Throws() {
+    public void noNullElements_nullCollection_throws() {
         Verify.noNullElements((List<Point>) null);
     }
 
-    @Test(expected = NullPointerException.class)
-    public void noNullElements_NullElement_Throws() {
-        Verify.noNullElements(Lists.newArrayList("Hello", "World", null));
+    @Test
+    public void noNullElements_nullCollection_throwsWithMessage() {
+        String collectionName = "My Collection";
+        try {
+            Verify.nonEmpty((String) null, collectionName);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException expected) {
+            assertContains(expected.getMessage(), collectionName);
+        }
     }
 
     @Test
-    public void noNullElements_Succeeds() {
+    public void noNullElements_collectionWithNullElement_throws() {
+        noNullElements_collectionWithNullElement_throws(Lists.newArrayList("", "hi", null), 2);
+        noNullElements_collectionWithNullElement_throws(Lists.newArrayList(null, "hi", null), 0);
+        noNullElements_collectionWithNullElement_throws(Lists.newArrayList("", null, "hi"), 1);
+        noNullElements_collectionWithNullElement_throws(Lists.newArrayList((String) null), 0);
+        noNullElements_collectionWithNullElement_throws(Lists.newArrayList("", null, null, ""), 1);
+    }
+
+    private void noNullElements_collectionWithNullElement_throws(Collection<?> collection,
+            int firstNullElementPos) {
+        try {
+            Verify.noNullElements(collection);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException expected) {
+            assertContainsIgnoreCase(expected.getMessage(), "element " + firstNullElementPos);
+        }
+    }
+
+    @Test
+    public void noNullElements_collectionWithNullElement_throwsWithMessage() {
+        noNullElements_collectionWithNullElement_throwsWithMessage(
+                Lists.newArrayList("", "hi", null), "My Collection", 2);
+        noNullElements_collectionWithNullElement_throwsWithMessage(
+                Lists.newArrayList(null, "hi", null), "My Collection", 0);
+        noNullElements_collectionWithNullElement_throwsWithMessage(
+                Lists.newArrayList("", null, "hi"), "My Collection", 1);
+        noNullElements_collectionWithNullElement_throwsWithMessage(
+                Lists.newArrayList((String) null), "My Collection", 0);
+        noNullElements_collectionWithNullElement_throwsWithMessage(
+                Lists.newArrayList("", null, null, ""), "My Collection", 1);
+    }
+
+    private void noNullElements_collectionWithNullElement_throwsWithMessage(
+            Collection<?> collection, String collectionName, int firstNullElementPos) {
+        try {
+            Verify.noNullElements(collection, collectionName);
+            fail("Expected NullPointerException");
+        } catch (NullPointerException expected) {
+            String message = expected.getMessage();
+            assertContainsIgnoreCase(message, "element " + firstNullElementPos);
+            assertContains(message, collectionName);
+        }
+    }
+
+    @Test
+    public void noNullElements_succeeds() {
+        // None of these should thow.
+        Verify.noNullElements(Lists.newArrayList());
+        Verify.noNullElements(Lists.newArrayList(""));
+        Verify.noNullElements(Lists.newArrayList("Hello"));
         Verify.noNullElements(Lists.newArrayList("Hello", "World", ""));
     }
 }
