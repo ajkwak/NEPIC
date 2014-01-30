a63fc2707f56150b77f2e969cc0f2dbf5f0f280b
ajkwak <ajkwak@users.noreply.github.com>
2014-01-29 19:48:01 -0800

    Add javadoc to BoundedRegion

--- src/nepic/geo/BoundedRegion.java
+++ src/nepic/geo/BoundedRegion.java
@@ -1,22 +1,52 @@
 package nepic.geo;
 
 /**
- * 
+ * A region in two-dimensional Cartesian space that can be
+ *
  * @author AJ Parmidge
- * @since AutoCBFinder_Alpha_v0-9-2013-02-10
- * @version AutoCBFinder_Alpha_v0-9-2013-02-10
- * 
  */
 public interface BoundedRegion {
+    /**
+     * Returns the minimum x-coordinate value in the {@link BoundedRegion}.
+     */
     public int getMinX();
 
+    /**
+     * Returns the maximum x-coordinate value in the {@link BoundedRegion}.
+     */
     public int getMaxX();
 
+    /**
+     * Returns the minimum y-coordinate value in the {@link BoundedRegion}.
+     */
     public int getMinY();
 
+    /**
+     * Returns the maximum y-coordinate value in the {@link BoundedRegion}.
+     */
     public int getMaxY();
 
+    /**
+     * Determine whether the bounds of this {@link BoundedRegion} inclusively contain the bounds of
+     * the parameter {@link BoundedRegion}.
+     * <p>
+     * NOTE: since this method returns whether or not the bounds of this {@link BoundedRegion}
+     * contain the bounds of the given {@link BoundedRegion}, this method should return {@code true}
+     * on the call {@code boundsContain(this)}.
+     *
+     * @param region the potentially contained bounded region
+     * @return {@code true} if the bounds of {@code this} {@link BoundedRegion} contain the bounds
+     *         of the given {@link BoundedRegion}; otherwise {@code false}
+     */
     public boolean boundsContain(BoundedRegion region);
 
+    /**
+     * Determine whether the bounds of this {@link BoundedRegion} inclusively contain the given
+     * coordinate.
+     *
+     * @param region the potentially contained bounded region
+     * @return {@code true} if the bounds of {@code this} {@link BoundedRegion} contain the bounds
+     *         of the given {@link BoundedRegion}; otherwise {@code false}
+     */
     public boolean boundsContain(int x, int y);
 }
