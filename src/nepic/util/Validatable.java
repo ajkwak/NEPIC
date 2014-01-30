5ea929ab492bded79794d84498e80397ad678474
ajkwak <ajkwak@users.noreply.github.com>
2014-01-29 16:45:50 -0800

    Add javadoc to Validatable interface

--- src/nepic/util/Validatable.java
+++ src/nepic/util/Validatable.java
@@ -1,7 +1,17 @@
 package nepic.util;
 
+/**
+ * Interface for objects that can exit in a valid or a non-valid state.
+ * 
+ * @author AJ Parmidge
+ */
 public interface Validatable {
 
+    /**
+     * Determines whether this object is currently in a valid state.
+     *
+     * @return {@code true} if this object is in a currently valid state; otherwise {@code false}
+     */
     public boolean isValid();
 
 }
