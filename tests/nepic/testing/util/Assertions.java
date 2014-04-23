485de0ad2f98b624a2b814963877c85883e835cd
ajkwak <ajkwak@users.noreply.github.com>
2014-04-23 08:44:08 -0700

    Add and test some new assertions to Assertions

--- tests/nepic/testing/util/Assertions.java
+++ tests/nepic/testing/util/Assertions.java
@@ -1,8 +1,15 @@
 package nepic.testing.util;
 
-import java.util.Collection;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import junit.framework.AssertionFailedError;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 /**
  * This class contains a collection of static assert methods with helpful fail errors. This class
@@ -20,8 +27,7 @@ public class Assertions {
      */
     public static void assertContains(String string, String substring) {
         if (string == null || substring == null || !string.contains(substring)) {
-            throw new AssertionFailedError("Expected \"" + string + "\" to contain substring \""
-                    + substring + "\"");
+            fail("Expected \"" + string + "\" to contain substring \"" + substring + "\"");
         }
     }
 
@@ -34,8 +40,7 @@ public class Assertions {
     public static void assertContainsIgnoreCase(String string, String substring) {
         if (string == null || substring == null
                 || !string.toLowerCase().contains(substring.toLowerCase())) {
-            throw new AssertionFailedError("Expected \"" + string + "\" to contain substring \""
-                    + substring + "\"");
+            fail("Expected \"" + string + "\" to contain substring \"" + substring + "\"");
         }
     }
 
@@ -47,8 +52,102 @@ public class Assertions {
      */
     public static <E> void assertContains(E element, Collection<E> collection) {
         if (collection == null || !collection.contains(element)) {
-            throw new AssertionFailedError("Expected " + collection + " to contain element <"
-                    + element + ">");
+            fail("Expected to find <" + element + "> in <" + collection + ">");
+        }
+    }
+
+    /**
+     * Asserts that the given {@link Collection} does NOT contain the given element.
+     *
+     * @param element the element to check
+     * @param collection the collection to check
+     */
+    public static <E> void assertNotContains(E element, Collection<E> collection) {
+        if (collection != null && collection.contains(element)) {
+            fail("Expected to NOT find <" + element + "> in <" + collection + ">");
+        }
+    }
+
+    /**
+     * Asserts that the two {@link Collection}s contain exactly the same elements, but those
+     * elements may be ordered differently.
+     *
+     * @param c1 the first collection to check
+     * @param c2 the second collection to check
+     */
+    public static <E> void assertEqualsAnyOrder(Collection<E> c1, Collection<E> c2) {
+        if (c1 == null && c2 == null) {
+            return; // They are equal.
+        } else if (c1 == null ^ c2 == null) {
+            fail("One of the given collections is null.");
+        }
+        assertEquals("Given collections have different lengths!  c1.size = " + c1.size()
+                + ", c2.size = " + c2.size(), c1.size(), c2.size());
+
+        // Make an element hashmap of c1. This maps <element> --> <number of instances in c1>
+        HashMap<E, Integer> c1Map = Maps.newHashMap();
+        for (E element : c1) {
+            Integer numInstances = c1Map.get(element);
+            if (numInstances == null) {
+                numInstances = 0;
+            }
+            c1Map.put(element, numInstances + 1);
+        }
+
+        // Make an element hashmap of c2.
+        HashMap<E, Integer> c2Map = Maps.newHashMap();
+        for (E element : c2) {
+            Integer numInstances = c2Map.get(element);
+            if (numInstances == null) {
+                numInstances = 0;
+            }
+            c2Map.put(element, numInstances + 1);
+        }
+
+        // Get the union of the keySets of the two HashMaps.
+        Set<E> keySetUnion = Sets.newHashSet(c1Map.keySet());
+        keySetUnion.addAll(c2Map.keySet()); // This should return false if c1 and c2 are equal.
+
+        // Verify that the hashmaps are the same.
+        for (E key : keySetUnion) {
+            Integer numInstancesC1 = c1Map.get(key);
+            if (numInstancesC1 == null) {
+                numInstancesC1 = 0;
+            }
+            Integer numInstancesC2 = c2Map.get(key);
+            if (numInstancesC2 == null) {
+                numInstancesC2 = 0;
+            }
+            if (numInstancesC1 != numInstancesC2) {
+                if (numInstancesC1 > numInstancesC2) {
+                    fail("1st collection contains more instances of <" + key
+                            + "> than the 2nd collection");
+                } else {
+                    fail("1st collection contains fewer instances of <" + key
+                            + "> than the 2nd collection");
+                }
+            }
+        }
+    }
+
+    /**
+     * Asserts that the two given collections contain no equivalent elements.
+     *
+     * @param c1 the first collection to check
+     * @param c2 the second collection to check
+     */
+    public static <E> void assertDistinct(Collection<E> c1, Collection<E> c2) {
+        // Make an element hash set of c1.
+        HashSet<E> c1HashSet = Sets.newHashSet();
+        for (E element : c1) {
+            c1HashSet.add(element);
+        }
+
+        // Check the elements in c2 against this has set.
+        for (E element : c2) {
+            if (c1HashSet.contains(element)) {
+                fail("Both collections contain the element <" + element + ">");
+            }
         }
     }
 }
--- tests/nepic/testing/util/AssertionsTest.java
+++ tests/nepic/testing/util/AssertionsTest.java
@@ -1,15 +1,12 @@
 package nepic.testing.util;
 
-import static org.junit.Assert.fail;
-
 import java.awt.Point;
 import java.util.Collection;
-
-import junit.framework.AssertionFailedError;
 
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 public class AssertionsTest {
 
@@ -21,22 +18,22 @@ public class AssertionsTest {
         Assertions.assertContains("Hello, world!", "world");
     }
 
-    @Test(expected = AssertionFailedError.class)
+    @Test(expected = AssertionError.class)
     public void assertContains_nullString_throws() {
         Assertions.assertContains(null, "Hi!");
     }
 
-    @Test(expected = AssertionFailedError.class)
+    @Test(expected = AssertionError.class)
     public void assertContains_nullSubstring_throws() {
         Assertions.assertContains("Hi!", (String) null);
     }
 
-    @Test(expected = AssertionFailedError.class)
+    @Test(expected = AssertionError.class)
     public void assertContains_stringDoesNotContainSubstring_throws() {
         Assertions.assertContains("Hello, world!", "Goodbye");
     }
 
-    @Test(expected = AssertionFailedError.class)
+    @Test(expected = AssertionError.class)
     public void assertContains_stringContainsNonCaseSensitiveSubstring_throws() {
         Assertions.assertContains("Hello, world!", "hello");
     }
@@ -50,17 +47,17 @@ public class AssertionsTest {
         Assertions.assertContainsIgnoreCase("Hello, world!", "o, wOrLd");
     }
 
-    @Test(expected = AssertionFailedError.class)
+    @Test(expected = AssertionError.class)
     public void assertContainsIgnoreCase_stringDoesNotContainSubstring_throws() {
         Assertions.assertContainsIgnoreCase("Hello, world!", "Once");
     }
 
-    @Test(expected = AssertionFailedError.class)
+    @Test(expected = AssertionError.class)
     public void assertContainsIgnoreCase_nullString_throws() {
         Assertions.assertContainsIgnoreCase(null, "Hi!");
     }
 
-    @Test(expected = AssertionFailedError.class)
+    @Test(expected = AssertionError.class)
     public void assertContainsIgnoreCase_nullSubstring_throws() {
         Assertions.assertContainsIgnoreCase("Hello, world!", null);
     }
@@ -74,28 +71,220 @@ public class AssertionsTest {
         Assertions.assertContains(new Point(5, 5), Lists.newArrayList(new Point(5, 5)));
     }
 
+    @Test
     public void assertContains_collectionDoesNotContainElement_throws() {
         // CASE: Null collection.
         try {
             Assertions.assertContains("Hello", (Collection<String>) null);
-            fail("Expected AssertionFailedError.");
-        } catch (AssertionFailedError expected) {
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
             // Expected.
         }
 
         // CASE: Empty collection.
         try {
             Assertions.assertContains(true, Lists.newArrayList());
-            fail("Expected AssertionFailedError.");
-        } catch (AssertionFailedError expected) {
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
             // Expected.
         }
 
         // CASE: Non-empty collection lacking desired element.
         try {
             Assertions.assertContains("Hello", Lists.newArrayList("Hi", "world!"));
-            fail("Expected AssertionFailedError.");
-        } catch (AssertionFailedError expected) {
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+    }
+
+    @Test
+    public void assertNotContains_succeeds() {
+        // CASE: Collection is null.
+        Assertions.assertNotContains("Hello", (Collection<String>) null);
+
+        // CASE: Empty collection.
+        Assertions.assertNotContains(true, Lists.newArrayList());
+
+        // CASE: Non-empty collection lacking desired element.
+        Assertions.assertNotContains(1, Lists.newArrayList(0, 2, -9, 7, 5, 12));
+    }
+
+    @Test
+    public void assertNotContains_collectionContainsElement_throws() {
+        // CASE: Given element is only element.
+        try {
+            Assertions.assertNotContains("Hello", Lists.newArrayList("Hello"));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+
+        // CASE: Given element is first element.
+        try {
+            Assertions.assertNotContains(true, Lists.newArrayList(true, false, false));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+
+        // CASE: Given element is in the middle of the collection.
+        try {
+            Assertions.assertNotContains(5, Lists.newArrayList(9, -3, 0, 8, 5, 8, 3, 2, 0, -14));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+
+        // CASE: Given element is the last element.
+        try {
+            Assertions.assertNotContains("Hello", Lists.newArrayList("Hi", "world!", "Hello"));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+
+        // CASE: Given element appears multiple times in the collection.
+        try {
+            Assertions.assertNotContains(false,
+                    Lists.newArrayList(true, false, false, true, false, true, false, true, true));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+    }
+
+    @Test
+    public void assertEqualsAnyOrder_succeeds() {
+        // CASE: Both collections are null.
+        Assertions.assertEqualsAnyOrder((Collection<Object>) null, (Collection<Object>) null);
+
+        // CASE: Both collections are empty.
+        Assertions.assertEqualsAnyOrder(Lists.newLinkedList(), Lists.newArrayList());
+
+        // CASE: Collections have the same elements in the same order.
+        Assertions.assertEqualsAnyOrder(Lists.newArrayList("Hi"), Lists.newArrayList("Hi"));
+        Assertions.assertEqualsAnyOrder(Lists.newArrayList(0, 2, -9, 7, 5, 12),
+                Lists.newArrayList(0, 2, -9, 7, 5, 12));
+
+        // CASE: Collections have the same elements different orders.
+        Assertions.assertEqualsAnyOrder(Lists.newArrayList("Hello", "world"),
+                Lists.newArrayList("world", "Hello"));
+        Assertions.assertEqualsAnyOrder(Lists.newArrayList(1, 2, 5, 2, 6, 7, 9, 0, 2),
+                Lists.newArrayList(0, 1, 2, 2, 2, 5, 6, 7, 9));
+    }
+
+    @Test
+    public void assertEqualsAnyOrder_oneCollectionNull_throws() {
+        // CASE: 2nd collection is null.
+        try {
+            Assertions.assertEqualsAnyOrder(Lists.newArrayList(2, 5), (Collection<Integer>) null);
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+
+        // CASE: 1st collection is null.
+        try {
+            Assertions.assertEqualsAnyOrder((Collection<Object>) null, Lists.newArrayList());
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+    }
+
+    @Test
+    public void assertEqualsAnyOrder_collectionsHaveDifferentNumElements_throws() {
+        // CASE: 1st collection has more elements than 2nd collection.
+        try {
+            Assertions.assertEqualsAnyOrder(Lists.newArrayList(2, 5), Lists.newArrayList(5));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+
+        // CASE: 2nd collection has more elements than 1st collection.
+        try {
+            Assertions.assertEqualsAnyOrder(Lists.newArrayList("Bwah", "Mal", "Inara"),
+                    Lists.newArrayList("Bwah", "Inara", "Mal", "Inara"));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+    }
+
+    @Test
+    public void assertEqualsAnyOrder_collectionsContainDifferentElements_throws() {
+        // CASE: Collections are distinct.
+        try {
+            Assertions.assertEqualsAnyOrder(Lists.newArrayList(1, 2, 3, 4),
+                    Lists.newArrayList(5, 6, 7, 8));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+
+        // CASE: Collections contain only some of the same elements.
+        try {
+            Assertions.assertEqualsAnyOrder(Lists.newArrayList(1, 2, 3, 4),
+                    Lists.newArrayList(1, 2, 1, 3));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+
+        // CASE: The collections contain the same elements, but different numbers of repetitions of
+        // each element.
+        try {
+            Assertions.assertEqualsAnyOrder(Lists.newArrayList(1, 2, 2, 3, 4),
+                    Lists.newArrayList(1, 1, 2, 3, 4));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+    }
+
+    @Test
+    public void assertDistinct_succeeds() {
+        // CASE: Both collections are empty.
+        Assertions.assertDistinct(Lists.newArrayList(), Sets.newHashSet());
+
+        // CASE: One of the collections is empty.
+        Assertions.assertDistinct(Lists.newArrayList(), Sets.newHashSet());
+
+        // CASE: Both collections are non-empty, but do not contain the same elements.
+        Assertions.assertDistinct(Lists.newArrayList(1, 1, 4, 5), Sets.newHashSet(2, 2, 3));
+        Assertions.assertDistinct(Lists.newArrayList("Hello", "Hi"),
+                Lists.newArrayList("HelloHi", "world"));
+        Assertions.assertDistinct(Lists.newArrayList(true), Sets.newHashSet(false));
+    }
+
+    @Test
+    public void assertDistinct_throws() {
+        // CASE: The collections share an element.
+        try {
+            Assertions.assertDistinct(Lists.newArrayList(1, 2, 2, 3, 4),
+                    Lists.newArrayList(6, 0, 3, 5, -3));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+
+        // CASE: The collections share multiple elements.
+        try {
+            Assertions.assertDistinct(Lists.newArrayList(9, 1, 2, 5, 2, 5, 7, 9),
+                    Lists.newArrayList(0, 1, 1, 2, 3, 4, 6));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
+            // Expected.
+        }
+
+        // CASE: The collections are the same.
+        try {
+            Assertions.assertDistinct(Lists.newArrayList(5, 4, 3, 2, 1),
+                    Lists.newArrayList(5, 4, 3, 2, 1));
+            throw new IllegalStateException("Expected AssertionError.");
+        } catch (AssertionError expected) {
             // Expected.
         }
     }
